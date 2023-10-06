ac5b01f28750b579f49c705ef8572cabf258c6d6
cmelgarejo <cmelgarejo.dev@gmail.com>
2023-10-06 10:59:05 -0300

    change to fastify!
    Signed-off-by: cmelgarejo <cmelgarejo.dev@gmail.com>

--- apps/server/src/filters/exception.filter.ts
+++ apps/server/src/filters/exception.filter.ts
@@ -1,15 +1,15 @@
 import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from "@nestjs/common";
-import { Request, Response } from "express";
+import { FastifyRequest, FastifyReply } from "fastify";
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
+    const response = ctx.getResponse<FastifyReply>();
+    const request = ctx.getRequest<FastifyRequest>();
     const status = exception.getStatus();
 
-    response.status(status).json({
+    response.status(status).send({
       statusCode: status,
       message: exception.message,
       timestamp: new Date().toISOString(),
--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,13 +2,14 @@ import "dotenv/config";
 import { NestFactory } from "@nestjs/core";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
-import { HttpExceptionFilter } from "./exception/exception.filter";
+import { HttpExceptionFilter } from "./filters/exception.filter";
 import { Logger } from "@nestjs/common";
+import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
 
 const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
   app.useGlobalFilters(new HttpExceptionFilter());
   // add swagger plugin to app
   const options = new DocumentBuilder()
@@ -19,7 +20,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   if (process.env.NODE_ENV === "development") SwaggerModule.setup("docs", app, document);
-  await app.listen(PORT);
+  await app.listen(PORT, "0.0.0.0");
   new Logger().log(`Server started on port ${PORT}`);
 }
 bootstrap();
